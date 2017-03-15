2c95499a43ef66ff9ed7ee5481746ca66b614ad2
Jorden Hill <jhill4756@gmail.com>
2017-03-14 21:23:27 -0400

    Start
    Converting from HTML/Javascript extension to to App Extension

---
+++ AppExtension/SafariExtensionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SafariExtensionViewController.swift
+//  AppExtension
+//
+//  Created by Jorden Hill on 3/14/17.
+//  Copyright © 2017 Jorden Hill. All rights reserved.
+//
+
+import SafariServices
+
+class SafariExtensionViewController: SFSafariExtensionViewController {
+    
+    static let shared = SafariExtensionViewController()
+
+}
